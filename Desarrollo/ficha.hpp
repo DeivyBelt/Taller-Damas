b35cfac887ae6f3541d2726de51ee0639af1fee8
karen59-uni <sofiabenites996@gmail.com>
2025-11-10 15:24:09 -0500

    base de ficha.hpp

--- Desarrollo/ficha.hpp
+++ Desarrollo/ficha.hpp
@@ -1,1 +1,24 @@
+#ifdef FICHA_HPP
+#define FICHA_HPP
 
+#include <string>
+
+class ficha {
+int fila; 
+int columna; 
+bool coronoDama; 
+int jugador; 
+std::string color; 
+char simbolo;
+
+ficha(int fila=0, int columna=0, int jugador=0, char simbolo= "o", std::string color="\033[37m");
+
+virtual bool mover (int nuevafila, int nuevacolumna)=0
+virtual bool hayCaptura ()=0
+
+int fila() const;
+int columna() const;
+bool coronoDama() const;
+int jugador() const;
+std::string color() const;
+char simbolo() const;
