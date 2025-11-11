4ecabab8eead71368a5e2f0d488f8994a9b97c6f
karen59-uni <sofiabenites996@gmail.com>
2025-11-10 22:35:29 -0500

    basedama

--- Desarrollo/Damas.hpp
+++ Desarrollo/Damas.hpp
@@ -1,1 +1,12 @@
+#ifndef DAMA_HPP
+#define DAMA_HPP
+#include "ficha.hpp"
+
+class Dama:ficha {
+public:  
+  Dama(int f=0, int c=0, int j=0, string col="\033[");
+
+bool mover(int nuevafila, int nuevacolumna)
+bool haycaptura;
+};
 
