452cd822d33fef0d6439c2753ba83bcd547b5b8d
karen59-uni <sofiabenites996@gmail.com>
2025-11-11 18:44:14 -0500

    ....

---
+++ Desarrollo/Ficha.cpp
@@ -0,0 +1,46 @@
+#include "Ficha.hpp"
+#include <iostream>
+using namespace std;
+
+Ficha::Ficha(int f, int c, int j, char s, string cor){
+  fila=f;
+  columna=c;
+  jugador=j;
+  simbolo=s;
+  color=cor;
+  coronoDama=false;
+}
+
+int Ficha::getFila() const {
+  return fila;
+}
+int Ficha::getColumna() const {
+  return columna;
+}
+bool Ficha::getCoronoDama() const {
+  return coronoDama;
+}
+int Ficha::getJugador() const {
+  return jugador;
+}
+string Ficha::getColor() const {
+  return color;
+}
+char Ficha::getSimbolo() const {
+  return simbolo;
+}
+
+void Ficha::setPosicion(int nuevaFila, int nuevaColumna){
+  fila=nuevaFila;
+  columna=nuevaColumna;
+}
+void Ficha::coronar(){
+  coronoDama=true;
+}
+void Ficha::setColor(string nuevoColor){
+  color=nuevoColor;  //color diferente para ficha
+}
+
+void Ficha::mostrar(){
+  cout << color << simbolo << "\033[0m";
+}
