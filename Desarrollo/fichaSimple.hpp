de47daa43ecedc15d9f387105033c3796e813574
karen59-uni <sofiabenites996@gmail.com>
2025-11-10 19:50:45 -0500

    ficha incial

--- Desarrollo/fichaSimple.hpp
+++ Desarrollo/fichaSimple.hpp
@@ -1,1 +1,15 @@
+#ifndef FICHASIMPLE_HPP
+#define FICHASIMPLE_HPP
+
+#include "ficha.hpp"
+
+class fichaSimple:public ficha {
+public:
+fichaSimple(int f=0, int c=0, int j=0, char s= 'o', std::string cor="\033[37m");
+
+bool mover (int nuevafila, int nuevacolumna);
+bool hayCaptura ();
+};
+#endif
+
 
