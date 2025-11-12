c4c90a17fee49880587b79a051426fe44770114b
DeivyBelt <deivybel.h@gmail.com>
2025-11-11 23:05:53 -0500

    Función de validación y acción del movimiento de la dama

--- Desarrollo/FichaDama.cpp
+++ Desarrollo/FichaDama.cpp
@@ -5,3 +5,15 @@
 FichaDama::FichaDama(int f, int c, int j, char s, std::string col)
     : Ficha(f, c, j, s, col) { esDama = true; }
 
+bool FichaDama::mover(int nuevaFila, int nuevaColumna) {
+    int difFila = std::abs(nuevaFila - fila);
+    int difColumna = std::abs(nuevaColumna - columna);
+    if (difFila == difColumna && difFila > 0) {
+    fila = nuevaFila;
+    columna = nuevaColumna;
+    return true;
+    }
+    return false;
+}
+
+
