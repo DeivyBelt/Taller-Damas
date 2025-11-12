b070b129badfefcca432343f3164a318e6cce8b0
karen59-uni <sofiabenites996@gmail.com>
2025-11-12 18:11:26 -0500

    logica de captura dama

--- Desarrollo/FichaDama.cpp
+++ Desarrollo/FichaDama.cpp
@@ -3,7 +3,9 @@
 #include <iostream>
 
 FichaDama::FichaDama(int f, int c, int j, char s, std::string cor)
-    : Ficha(f, c, j, s, cor) { esDama = true; }
+    : Ficha(f, c, j, s, cor) { 
+        coronoDama = true; 
+    }
 
 bool FichaDama::mover(int nuevaFila, int nuevaColumna) {
     int difFila = std::abs(nuevaFila - fila);
@@ -19,6 +21,24 @@ bool FichaDama::mover(int nuevaFila, int nuevaColumna) {
 bool FichaDama::hayCaptura() {
     return false; 
 }
+
+bool FichaDama::puedeCapturarHacia(int filaDestino, int colDestino, int& filaCap, int& colCap) {
+    int difFila = filaDestino - fila;
+    int difColumna = colDestino - columna;
+
+    if (abs(difFila) != abs(difColumna) || difFila == 0) {
+        return false;
+    }
+
+    int dirFila = (difFila > 0) ? 1 : -1;
+    int dirCol = (difColumna > 0) ? 1 : -1;
+
+    filaCap = fila + dirFila;
+    colCap = columna + dirCol;
+
+    return true;    
+}
+
 
 
 
