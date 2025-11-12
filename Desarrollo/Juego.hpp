a30ff5d8d6f54e28f8c767086ca25eed330b1957
karen59-uni <sofiabenites996@gmail.com>
2025-11-12 01:06:20 -0500

    estructura
    turnos, 
    finpartida y vict
    20 fchs cd jug
    menu

--- Desarrollo/Juego.hpp
+++ Desarrollo/Juego.hpp
@@ -1,1 +1,24 @@
+#ifndef JUEGO_HPP
+#define JUEGO_HPP
+#include "Tablero.hpp"
+#include <iostream>
 
+class Juego {
+
+    Tablero* tablero;       
+    int turno;              
+    bool partidaEnCurso;     
+
+    void mostrarMenu();
+    void mostrarReglas();
+    void cambiarTurno();
+    bool verificarVictoria();
+
+   Juego();
+   Juego();
+
+void iniciar(); 
+void iniciarPartida()
+void jugarTurno()
+}
+endif
