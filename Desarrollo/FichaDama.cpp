035dfa3dd5107cb4a57776e56296dfe2824958fd
DeivyBelt <deivybel.h@gmail.com>
2025-11-11 22:53:18 -0500

    Constructor para determinar una ficha de tipo dama
    Added includes and constructor for FichaDama class.

--- Desarrollo/FichaDama.cpp
+++ Desarrollo/FichaDama.cpp
@@ -1,1 +1,7 @@
+#include "FichaDama.hpp"
+#include <cmath>
+#include <iostream>
+
+FichaDama::FichaDama(int f, int c, int j, char s, std::string col)
+    : Ficha(f, c, j, s, col) { esDama = true; }
 
