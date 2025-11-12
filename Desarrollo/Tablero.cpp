5700b5def31ae004a0865a946861915db830329e
karen59-uni <sofiabenites996@gmail.com>
2025-11-12 00:52:15 -0500

    organicion
    funciones
    verficaciond de coronar

--- Desarrollo/Tablero.cpp
+++ Desarrollo/Tablero.cpp
@@ -45,15 +45,15 @@ void Tablero::inicializar() {
 
 void Tablero::mostrar() {
     std::cout << "\n    ";
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 10; j++) 
         std::cout << " " << j << " ";
-    }std::cout << "\n";
+        std::cout << "\n";
     
     for (int i = 0; i < 10; i++) {
         std::cout << " " << i << "  ";
         
-        for (int j = 0; j < 10; j++)
-            if ((i + j) % 2 == 0){
+        for (int j = 0; j < 10; j++){
+            if ((i + j) % 2 == 0)
                 std::cout << colorClaro << "   ";}
             else
                 {std::cout << colorOscuro << "   ";}
@@ -67,16 +67,43 @@ void Tablero::mostrar() {
         }
         std::cout << "\033[0m\n";
     }
-    std::cout << "\n";
+}
+bool Tablero::posicionValida(int fila, int col) const {
+    return fila >= 0 && fila < 10 && col >=0 && col < 10;
+}
+
+bool Tablero::casillaValida(int fila, int col) const {
+    return casillas [fila][col]==nullptr;
+}
+
+int Tablero::propietarioDe(int fila, int col) const {
+ if (!posicionValida(fila, col) || casillas[fila][col] == nullptr)
+        return 0;
+    return casillas[fila][col]->getJugador();
 }
 
 bool Tablero::moverFicha(int filaOrigen, int colOrigen, int filaDestino, int colDestino) {
+    if (!posicionValida(filaOrigen, colOrigen) || !posicionValida(filaDestino, colDestino))
+        return false;
+
     if (casillas[filaOrigen][colOrigen] == nullptr)
         return false;
+
+    if (!casillaVacia(filaDestino, colDestino))
+        return false;
+    
     Ficha* ficha = casillas[filaOrigen][colOrigen];
     if (ficha->mover(filaDestino, colDestino)) {
         casillas[filaDestino][colDestino] = ficha;
         casillas[filaOrigen][colOrigen] = nullptr;
+
+    if (ficha->getJugador() == 1 && filaDestino == 9 && !ficha->getCoronoDama()) {
+            ficha->coronar();
+            cout << "Ficha del jugador 1 coronada\n";
+        } else if (ficha->getJugador() == 2 && filaDestino == 0 && !ficha->getCoronoDama()) {
+            ficha->coronar();
+            cout << "Ficha del jugador 2 coronada\n";
+        }
         return true;
     }
     return false;
