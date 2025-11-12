6e436a97b4097d2cfbc3007be0f33eae91b211f7
DeivyBelt <deivybel.h@gmail.com>
2025-11-11 23:44:48 -0500

    Constructor para ficha simple y función de movimiento

--- Desarrollo/fichaSimple.cpp
+++ Desarrollo/fichaSimple.cpp
@@ -1,1 +1,17 @@
+#include "FichaSimple.hpp"
+#include <iostream>
+#include <cmath>
 
+FichaSimple::FichaSimple(int f, int c, int j, char s, std::string cor)
+    : Ficha(f, c, j, s, cor) {}
+
+bool FichaSimple::mover(int nuevaFila, int nuevaColumna) {
+    int difFila = nuevaFila - fila;
+    int difColumna = nuevaColumna - columna;
+    if (jugador == 1) {
+        if (difFila == 1 && std::abs(difColumna) == 1) {
+            fila = nuevaFila;
+            columna = nuevaColumna;
+            return true;
+        }
+    }
