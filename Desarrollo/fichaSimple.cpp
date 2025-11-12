93af157d4980c26013331be463aae799bf1d215d
karen59-uni <sofiabenites996@gmail.com>
2025-11-12 17:53:24 -0500

    captura

--- Desarrollo/fichaSimple.cpp
+++ Desarrollo/fichaSimple.cpp
@@ -8,23 +8,37 @@ FichaSimple::FichaSimple(int f, int c, int j, char s, std::string cor)
 bool FichaSimple::mover(int nuevaFila, int nuevaColumna) {
     int difFila = nuevaFila - fila;
     int difColumna = nuevaColumna - columna;
-    if (jugador == 1) {
-        if (difFila == 1 && std::abs(difColumna) == 1) {
-        fila = nuevaFila;
-        columna = nuevaColumna;
-        return true;
+    
+    if (abs(difFila)==1 && abs (difColumna)== 1) {
+        if (jugador==1 && difFila > 0) {
+            fila = nuevaFila;
+            columna = nuevaColumna;
+            return true;
+        }
+
+    else if (jugador == 2 && difFila < 0) {
+            fila = nuevaFila;
+            columna = nuevaColumna;
+            return true;
         }
     }
-  else if (jugador == 2) {
-        if (difFila == -1 && std::abs(difColumna) == 1) {
-        fila = nuevaFila;
-        columna = nuevaColumna;
-        return true;
-    }
-}
-return false;
+    return false;
 }
 
 bool FichaSimple::hayCaptura() {
-return false;
+    return false;
 }
+bool FichaSimple::puedeCapturarHacia(int filaDestino, int colDestino, int& filaCap, int& colCap){
+    int difFila=filaDestino-fila;
+    int difColumna=colDestino-columna;
+
+    if (abs(difFila)==2&&abs(difColumna)==2){
+        filaCap=fila+difFila/2;
+        colCap=columna+difColumna/2;
+
+        if (jugador==1&& difFila>0)return true;
+        if (jugador==2 && difFila<0)return true;
+    }
+    return false;
+}
+        
