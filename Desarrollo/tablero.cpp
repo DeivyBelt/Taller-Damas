bd488cbadda434c4b151c4de6efb64d695942e2f
DeivyBelt <deivybel.h@gmail.com>
2025-11-11 22:08:54 -0500

    Colores del tablero

--- Desarrollo/tablero.cpp
+++ Desarrollo/tablero.cpp
@@ -1,1 +1,23 @@
+#include "Tablero.hpp"
+#include <iostream>
+
+Tablero::Tablero(std::string tipoColor) {
+    if (tipoColor == "clasico") {
+        colorClaro = "\033[47m";
+        colorOscuro = "\033[40m";
+    } else if (tipoColor == "rosa") {
+        colorClaro = "\033[105m";
+        colorOscuro = "\033[45m";
+    } else if (tipoColor == "azul") {
+        colorClaro = "\033[104m";
+        colorOscuro = "\033[44m";
+    } else if (tipoColor == "verde") {
+        colorClaro = "\033[102m";
+        colorOscuro = "\033[42m";
+    } else if (tipoColor == "morado") {
+        colorClaro = "\033[95m";
+        colorOscuro = "\033[35m";
+    }
+    inicializar();
+}
 
